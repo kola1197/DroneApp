be5fe6367a8dc82debc073344148dd641a67d9dc
kola1197 <kola-1197@mail.ru>
2020-06-13 22:28:28 +0300

    raspberry version fixes

---
+++ DroneOnboardController/build/CMakeFiles/DroneOnboardController.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DroneOnboardController.dir/main.cpp.o"
+  "CMakeFiles/DroneOnboardController.dir/StatsServer.cpp.o"
+  "CMakeFiles/DroneOnboardController.dir/CameraModule.cpp.o"
+  "CMakeFiles/DroneOnboardController.dir/DroneImage.cpp.o"
+  "CMakeFiles/DroneOnboardController.dir/DJI_utility.cpp.o"
+  "CMakeFiles/DroneOnboardController.dir/home/pi/Code/DroneApp/Utils/MutexBool.cpp.o"
+  "CMakeFiles/DroneOnboardController.dir/home/pi/Code/DroneApp/Utils/Messages.cpp.o"
+  "DroneOnboardController.pdb"
+  "DroneOnboardController"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DroneOnboardController.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
