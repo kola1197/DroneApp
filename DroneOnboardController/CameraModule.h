031ceeb26cf456b3a72accb07f708a6173d69b22
kola1197 <kola-1197@mail.ru>
2021-02-17 18:26:48 +0300

    guidance fix

--- DroneOnboardController/CameraModule.h
+++ DroneOnboardController/CameraModule.h
@@ -11,10 +11,10 @@
 #include "DroneImage.h"
 #include "../Utils/MutexBool.h"
 #include "../Utils/AsyncVar.h"
-#ifdef __arm__
-#include "DJI_guidance.h"
-#include "DJI_utility.h"
-#endif
+//#ifdef __arm__
+//#include "DJI_guidance.h"
+//#include "DJI_utility.h"
+//#endif
 
 class CameraModule {
 
@@ -36,25 +36,25 @@ public:
     AsyncVar<bool> imageForOdometryModuleUpdated{false};
     AsyncVar<int> frameNum{0};
 
-#ifdef __arm__                                                           //dji modules installed only on raspberry pi
-    static int my_callback(int data_type, int data_len, char *content);
-    static cv::Mat g_greyscale_image_left;
-    static cv::Mat	g_greyscale_image_right;
-    static e_vbus_index sensor_id;
-    static DJI_lock  g_lock;
-    static DJI_event g_event;
-    //const int WIDTH = 320;
-    //const int HEIGHT = 240;
-    //const int IMAGE_SIZE = 76800;
-#endif
+//#ifdef __arm__                                                           //dji modules installed only on raspberry pi
+//    static int my_callback(int data_type, int data_len, char *content);
+//    static cv::Mat g_greyscale_image_left;
+//    static cv::Mat	g_greyscale_image_right;
+//    static e_vbus_index sensor_id;
+//    static DJI_lock  g_lock;
+//    static DJI_event g_event;
+//    //const int WIDTH = 320;
+//    //const int HEIGHT = 240;
+//    //const int IMAGE_SIZE = 76800;
+//#endif
 private:
 
     MutexBool threadStop{false};
-#ifdef __arm__
-    int testMode = 0;      //1 - read images from video0/1 , 2 - from storage, 0 - dji system
-#else
+//#ifdef __arm__
+//    int testMode = 0;      //1 - read images from video0/1 , 2 - from storage, 0 - dji system
+//#else
     int testMode = 2;      //1 - read images from video0/1 , 2 - from storage, 0 - dji system, 3 both images from video0
-#endif
+//#endif
     void getDirectoryToSave();
     std::string dirToSave = "";
     int saveCounter=0;
